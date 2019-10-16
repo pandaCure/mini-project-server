2df707ec9ae13832b8b86938e4f570749808ec07
Jazk-Z <zzyamor@gmail.com>
2019-10-17 01:11:53 +0800

    feat: add body-paser

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const Koa = require('koa')
 const cors = require('koa2-cors')
 const ip = require('ip')
-const package = require('./package.json')
+const packages = require('./package.json')
 const app = new Koa()
+const parser = require('koa-bodyparser')
 const router = require('./router/index')
 const logger = require('./middleware/logger/logger.js')
 const handleError = require('./middleware/handleError/handleError')
+app.use(handleError)
 app.use(cors())
+app.use(parser())
 app.use(
   logger({
     env: process.env.NODE_ENV,
-    projectName: package.name,
+    projectName: packages.name,
     appLogLevel: 'info',
     dir: 'logs',
     serverIp: ip.address()
   })
 )
-app.use(handleError)
 router(app)
 module.exports = app
